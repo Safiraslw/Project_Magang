11caeac308c1158f8555392aa65e50b5a2594cbb
Safiraslw <90440818+Safiraslw@users.noreply.github.com>
2023-05-22 14:43:40 +0700

    commit

---
+++ dashboard.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap CSS -->
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" 
+    rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">-->
+    <link href="assets/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="CSS/styles.css" />
+    <title>Sistem Informasi Geografis</title>
+</head>
+
+<body>
+    <!-- Sidebar -->
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-3" style="background-color:aqua">
+                <?php require "sidebar.php"; ?>
+            </div>
+            <div class="col-9">
+                <center>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d100830.24172715456!2d97.04928600642612!3d5.16697531043998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x304778e967f2b613%3A0x3039d80b220cc20!2sLhokseumawe%2C%20Kota%20Lhokseumawe%2C%20Aceh!5e1!3m2!1sid!2sid!4v1675921007647!5m2!1sid!2sid" width="935" height="610" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </center>
+            </div>
+        </div>
+    </div>
+    <!-- Sidebar -->
+
+    <!-- Page level plugins -->
+    <script src="JavaScript/Chart.min.js"></script>
+
+    <!-- Page level custom scripts -->
+    <script src="assets/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="JavaScript/chart-pie-demo.js"></script>
+</body>
+
+</html>
---
+++ sidebar.php
@@ -0,0 +1,24 @@
+<!-- Sidbar -->
+<div class="bg-blue" id="sidebar-wrapper">
+    <div class="sidebar-heading text-center py-4 primary-text fs-4 fw-bold text-uppercase border-bottom">
+    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-lightning" viewBox="0 0 16 16">
+        <path d="M5.52.359A.5.5 0 0 1 6 0h4a.5.5 0 0 1 .474.658L8.694 6H12.5a.5.5 0 0 1 .395.807l-7 9a.5.5 0 0 1-.873-.454L6.823 9.5H3.5a.5.5 0 0 1-.48-.641l2.5-8.5zM6.374 1 4.168 8.5H7.5a.5.5 0 0 1 .478.647L6.78 13.04 11.478 7H8a.5.5 0 0 1-.474-.658L9.306 1H6.374z"/>
+    </svg>
+        GIS of Electricity Distribution
+    </div>
+    <div class="list-group list-group-flush">
+        <a href="dashboard.php" class="list-group-item list-group-item-action bg-transparent second-text fw-bold">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house-door-fill me-2" viewBox="0 0 16 16">
+                <path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5z" />
+            </svg>
+            Dashboard
+        </a>
+        <a href="to_do.php" class="list-group-item list-group-item-action bg-transparent second-text fw-bold">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
+            <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>
+        </svg>
+            Data Tiang
+        </a>
+    </div>
+</div>
+<!-- Akhir Sidebar -->
